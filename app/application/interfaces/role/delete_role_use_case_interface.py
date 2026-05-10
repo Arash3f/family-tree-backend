279e49c38822d35e5ef37e52a80e5c15bd4d2ace
arash alfooneh <arash.alfooneh@gmail.com>
2026-05-10 15:20:47 +0330

    :hammer: ref(core): Remove unnecessary implementations in application

--- app/application/interfaces/role/delete_role_use_case_interface.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from app.domain.shared.dto.common_dto import IdDTO, ResultDTO
-
-
-class IDeleteRoleUseCase(ABC):
-    @abstractmethod
-    async def execute(self, dto: IdDTO) -> ResultDTO:
-        raise NotImplementedError
